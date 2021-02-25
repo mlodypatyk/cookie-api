a565d67ce140e6044eb81307c2b91395c6f9442b
Piotr Olszewski <01156804@pw.edu.pl>
2021-02-25 18:53:09 +0100

    Memory optimisation

--- skyblock_api.py
+++ skyblock_api.py
@@ -12,15 +12,23 @@ def update():
     ah0 = requests.get(url + 'auctions', params = {'key': api_key})
     page0 = ah0.json()
     num_of_pages = page0["totalPages"] + 1
-    auctions_list += page0["auctions"]
+    #auctions_list += page0["auctions"]
 
     #for page_num in range(1, num_of_pages):
-    for page_num in range(1, num_of_pages):
+    for page_num in range(0, num_of_pages):
         print('Downloading page', page_num)
         page_n = requests.get(url + 'auctions', params = {'key': api_key, 'page': page_num})
         page_json = page_n.json()
         if page_json["success"]:
-            auctions_list += page_json["auctions"]
+            for auction in page_json["auctions"]:
+                auction_compact = {
+                    "item_name": auction["item_name"],
+                    "extra": auction["extra"]
+                }
+                if 'bin' in auction.keys():
+                    auction_compact['bin'] = True
+                    auction_compact['starting_bid'] = auction['starting_bid']
+                auctions_list.append(auction_compact)
         else:
             print('error')
     print('Finished updating')
